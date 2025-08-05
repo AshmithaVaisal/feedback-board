6f0bc89611165ee54756be8865720c3acb107834
Ashmitha <ashmithavaisal37@gmail.com>
2025-08-05 13:01:19 +0530

    Add Like button with usestate and conditional styling to FeedbackCard

--- client/src/components/FeedbackCard.jsx
+++ client/src/components/FeedbackCard.jsx
@@ -1,9 +1,27 @@
-const FeedbackCard = ({ username,message}) => {
-  return <>
-     <div className="bg-white p-6 rounded-2xl shadow-md border max-w-md">
-      <h2 className="text-xl font-semibold mb-2 text-gray-800">{username}</h2>
-      <p className="text-gray-600">{message}</p>
-    </div>
-  </>;
+import { useState } from "react";
+
+const FeedbackCard = ({ username, message }) => {
+  const [liked, setLiked] = useState(false);
+  const handleLike = () => {
+    setLiked(!liked);
+  };
+  return (
+    <>
+      <div className="bg-white p-6 rounded-2xl shadow-md border max-w-md">
+        <h2 className="text-xl font-semibold mb-2 text-gray-800">{username}</h2>
+        <p className="text-gray-600">{message}</p>
+        <button
+          onClick={handleLike}
+          className={`py-2 px-4 rounded transition ${
+            liked
+              ? "bg-green-500 hover:bg-green-600"
+              : "bg-blue-500 hover:bg-blue-600"
+          }text-white`}
+        >
+          {liked ? "💚 Liked" : "👍 Like"}
+        </button>
+      </div>
+    </>
+  );
 };
 export default FeedbackCard;
